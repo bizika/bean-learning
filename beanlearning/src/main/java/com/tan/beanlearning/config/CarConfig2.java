012573b43d7543e3c117f5ea9d1ec1146affde9f
bizika_xw <bizika_xw@126.com>
2018-06-25 17:42:42 +0800

    bean注解的上下文获取

---
+++ beanlearning/src/main/java/com/tan/beanlearning/config/CarConfig2.java
@@ -0,0 +1,11 @@
+package com.tan.beanlearning.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 601091
+ * @date 2018/6/25
+ */
+@Configuration
+public class CarConfig2 {
+}
