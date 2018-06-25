120ca093614c2153097e8eb20876a0bc7fefb9a2
bizika_xw <bizika_xw@126.com>
2018-06-25 14:26:15 +0800

    初始化

---
+++ beanlearning/src/main/java/com/tan/beanlearning/BeanlearningApplication.java
@@ -0,0 +1,12 @@
+package com.tan.beanlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanlearningApplication.class, args);
+    }
+}
---
+++ beanlearning/src/test/java/com/tan/beanlearning/BeanlearningApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tan.beanlearning;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeanlearningApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
