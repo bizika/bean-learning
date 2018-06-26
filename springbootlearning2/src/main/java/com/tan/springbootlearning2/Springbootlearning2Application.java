9c9c81bbc14ba148ee7870ae29025a0967ee7929
bizika_xw <bizika_xw@126.com>
2018-06-26 19:00:13 +0800

    初始化

---
+++ springbootlearning2/src/main/java/com/tan/springbootlearning2/Springbootlearning2Application.java
@@ -0,0 +1,12 @@
+package com.tan.springbootlearning2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlearning2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootlearning2Application.class, args);
+    }
+}
